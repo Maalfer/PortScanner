22356632f16f7937206b611d2bb7c4ea889082cc
Ruben Gomez <santiagogomezgarro@gmail.com>
2023-01-19 21:14:07 -0600

    update

--- portscanner.sh
+++ portscanner.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-read -p "Introduce tu dirección IP: " ip
-read -p "¿Cuantos puertos quieres examinar?: " ports
+finish() {
+    echo -e "\n[*] Cerrando el script..."
+    exit 0
+}
 
-for i in $(seq 0 $ports); do
-	sudo nc -zv $ip $i 2>/dev/null && echo "El puerto $i está abierto"
-done
+trap finish SIGINT
+
+read -p "Introduce la IP: " ip_address
+
+if [ "$ip_address" ]; then
+    for port in $(seq 1 65535); do
+        (echo > /dev/tcp/$ip_address/$port) 2>/dev/null && echo "[*] Puerto $port - Abierto" &
+    done
+    wait
+fi
